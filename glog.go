597f44fd2e82a9286be2af07e65f93450c1c0839
storezhang <storezhang@gmail.com>
2021-06-15 20:08:51 +0800

    集成盘古

--- glog.go
+++ glog.go
@@ -18,3 +18,13 @@ func New(opts ...option) (logger Logger, err error) {
 
 	return
 }
+
+// Must 必须返回日志
+func Must(opts ...option) (logger Logger) {
+	var err error
+	if logger, err = New(opts...); nil != err {
+		panic(err)
+	}
+
+	return
+}
