f84a6c4f3fd0e117caff299d50e0b2b19cdbb0db
storezhang <storezhang@gmail.com>
2022-12-06 20:14:54 +0800

    refactor(项目): 基于Builder设计模式重构
    [skip ci]

--- go.mod
+++ go.mod
@@ -13,3 +13,7 @@ require (
 	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 )
+
+replace (
+	github.com/goexl/gox => ../../goexl/gox
+)
--- simaqian_test.go
+++ simaqian_test.go
@@ -1,6 +1,7 @@
 package simaqian_test
 
 import (
+	"encoding/json"
 	"github.com/goexl/gox/field"
 	"github.com/goexl/simaqian"
 	"testing"
@@ -13,12 +14,13 @@ type jsonField struct {
 
 func TestZap(test *testing.T) {
 	smq := simaqian.Default()
-	smq.Info("测试字符串", field.New[string](`username`, `storezhang`))
-	smq.Info("测试字符串数组", field.New[[]string](`args`, []string{`--clean`, `test`, `deploy`}))
-	smq.Info("测试任意类型", field.New(`args`, []string{`--clean`, `test`, `deploy`}))
-	smq.Info("测试JSON", field.New(`json`, jsonField{
+	smq.Info("测试字符串", field.New[string]("username", "storezhang"))
+	smq.Info("测试字符串数组", field.New[[]string]("args", []string{"--clean", "test", "deploy"}))
+	smq.Info("测试任意类型", field.New("args", []string{"--clean", "test", "deploy"}))
+	smq.Info("测试JSON", field.New("json", jsonField{
 		Username: "storezhang",
 		Password: "password",
 	}))
+	smq.Info("测试JSON字符串", field.New("json.raw", json.RawMessage(`{"username":"storezhang  ","password":"password"}`)))
 	_ = smq.Sync()
 }
